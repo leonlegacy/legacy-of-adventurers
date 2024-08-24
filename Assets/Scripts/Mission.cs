9ca8fa1daafe96e4239d1ef02eb4e56a8edbc812
TachibanaNoSuke <TachibanaNoSuke@gmail.com>
2024-08-24 16:10:52 +0800

    [功能] 任務類別和任務管理

---
+++ Assets/Scripts/Mission.cs
@@ -0,0 +1,35 @@
+public class Mission
+{
+
+    // 任務名稱
+    public string Name;
+
+    // 任務描述
+    public string Description;
+
+    // 任務難度
+    public int Difficulty;
+
+    // 冒險人數需求
+    public int AdventurerCount;
+
+    // 遭遇數量
+    public int EncounterCount;
+
+    // 任務酬勞
+    public int Reward;
+
+    // 任務成功的抽成比例
+    public float SuccessRate;
+
+    // 任務成功的聲望值變化
+    public int SuccessReputation;
+
+    // 任務失敗的聲望值變化
+    public int FailReputation;
+
+    // 當前遭遇次數
+    public int CurrentEncounterTime;
+
+
+}
